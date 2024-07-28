5352f5c6177d28e38932c8e8893da30d9f266aea
Jordan Neal Pryor <jnpryor@student.fullsail.edu>
2024-07-28 15:22:13 -0500

    updated links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,7 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  github: "https://github.com/jordan-pryor",
+  linkedin: "https://www.linkedin.com/in/jordan-pryor/",
+  discord: "https://discordapp.com/users/510370989037715457",
 };
 
 export const loaderAnimation = [
